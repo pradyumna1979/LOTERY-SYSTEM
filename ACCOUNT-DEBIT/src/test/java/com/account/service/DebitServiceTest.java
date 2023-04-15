e554dcd89edc7c64f118ddeb8238de452e58cc93
pradyumna1979 <kuni.khadanga@gmail.com>
2023-04-15 08:30:10 +0530

    junit

--- ACCOUNT-DEBIT/src/test/java/com/account/service/DebitServiceTest.java
+++ ACCOUNT-DEBIT/src/test/java/com/account/service/DebitServiceTest.java
@@ -3,17 +3,11 @@ package com.account.service;
 import com.account.dto.DebitRequestObject;
 import com.account.repository.AccountRepository;
 import com.account.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.event.annotation.BeforeTestExecution;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 @SpringBootTest
@@ -29,7 +23,6 @@ public class DebitServiceTest {
     @DisplayName("debit test")
     @Test
     public void debitTest(){
-        debitRequestObject.setUserId(1);
         when(debitRequestObject.getUserId()).thenReturn(1l);
         System.out.println("userId" +debitRequestObject.getUserId());
         when(userRepository.findByUserId(1)).thenReturn(null);
